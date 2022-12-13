b8da108561cf75ac9c9b0e4a235c7eb2fc832d9d
Leia Soares <leia13soares@outlook.com>
2022-12-13 12:22:35 -0300

    Atualização, lista aparecendo e api sendo consumida

--- CrudSapUi5/wwwroot/controller/ListaDeLivros.controller.js
+++ CrudSapUi5/wwwroot/controller/ListaDeLivros.controller.js
@@ -2,41 +2,47 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator",
-	"sap/m/MessageToast"
-], function (Controller, JSONModel, Filter, FilterOperator, MessageToast) {
+	"sap/ui/model/FilterOperator"
+], function (Controller, JSONModel, Filter, FilterOperator) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.walkthrough.controller.ListaDeLivros", {
 
-		// onInit: function () {
-		// 	var router = sap.ui.core.UIComponent.getRouterFor(this);
-		// 	router.getRoute("telaCadastro").attachPatternMatched(this._Rota, this);
-			
-		// },
-
-		// _Rota: function (oEvent) {
-        //     this.byId().reset();
-		// 	this.getView().bindElement({
-		// 		path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").listaLivro),
-		// 		model: "listaDeLivros"
-		// 	});
-		// },
+		onInit: function () {
+			this.getOwnerComponent();
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("overview").attachPatternMatched(this.ajustarRota, this); //só será acionado para a rota que tiver um padrão correspondente
+		},
 		
+		ajustarRota: function () {
+			this.mostrarLista();
+		},
 
+
+		buscarLivros: async function (){
+			let livroASerBuscado;
+			 await fetch(`https://localhost:7278/CrudLivro`)
+			.then(response => response.json())
+			.then(data => livroASerBuscado = data)
+			
+			return livroASerBuscado;
+		},
+
+		mostrarLista: function(){
+			 var exibirLista = this.buscarLivros();
+			 exibirLista.then(lista => {
+				let oModel = new JSONModel(lista);
+				this.getView().setModel(oModel, "listaDeLivros");
+				
+			 })
+				
+
+		},
 		botaoCadastrar : function (){ //ao clicar no botão cadastrar
 			var oRouter = this.getOwnerComponent().getRouter();
 			oRouter.navTo("telaCadastro");
-			
 		},
-
-		buscarLivros: function (){
-			let livroASerBuscado = fetch(`https://localhost:7278/CrudLivro`)
-			.then(response => response.json())
-			.then(data => livroASerBuscado(data))
-			.catch(console.error('erro', error));
-			return livroASerBuscado;
-		}
+		
 
 	});
 });
