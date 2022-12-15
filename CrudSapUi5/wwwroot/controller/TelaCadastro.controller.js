337dfd9a7cdbcfec5b044c92fed1d32ba56969a1
Leia Soares <leia13soares@outlook.com>
2022-12-15 13:55:12 -0300

    atualização, tela de detalhes funcionando

--- CrudSapUi5/wwwroot/controller/TelaCadastro.controller.js
+++ CrudSapUi5/wwwroot/controller/TelaCadastro.controller.js
@@ -1,17 +1,49 @@
+// var url= fetch(`https://localhost:7278/CrudLivro ${id}`).then(response=> response.json()).then(data => console.log(data));
+// console.log(url);
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/core/routing/History",
 	"sap/ui/model/json/JSONModel",
-	"sap/m/MessageBox"
+	"sap/m/MessageBox",
+	"sap/ui/core/Core"
     
-], function (Controller, History, MessageBox, JSONModel) {
+], function (Controller, History, Core, MessageBox, JSONModel) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.walkthrough.controller.TelaCadastro", {
 
-		// var url= fetch(`https://localhost:7278/CrudLivro ${id}`).then(response=> response.json()).then(data => console.log(data));
-		// console.log(url);
+		
+		// onInit: function () {
+		// 	var router = sap.ui.core.UIComponent.getRouterFor(this);
+		// 	router.attachRoutePatternMatched(this.ajustarRota, this);
+		// 	var tela = this.getView(),
+		// 		registroDeLivros = Core.getMessageManager();
+		// 	registroDeLivros.registerObject(tela.byId("input-nome"), true)
+		// 	registroDeLivros.registerObject(tela.byId("input-autor"), true)
+		// 	registroDeLivros.registerObject(tela.byId("input-editora"), true) 
+		// },
+		
+		// ajustarRota: function () {
+		// 	var livrosCadastrados = oEvent.getParameter("arguments").id;
+		// 	this.mostrarLista(livrosCadastrados);
+		// 	this.getView().setModel(new sap.ui.model.json.JSONModel({}), "listaDeLivros");
+		// },
 
+		// buscarLivro: function (livroCadastrado) {
+		// 	let livroASerCadastrado= fetch(`https://localhost:7278/CrudLivro/${livroCadastrado}`)
+		// 	 	.then((response) => response.json())
+		// 		.then(data => livroASerCadastrado = data)
+		// 	return livroASerCadastrado;
+
+			
+		// },
+		// mostrarLista: function (livroCadastrado) {
+		// 	var exibirLista = this.buscarLivro(livroCadastrado)
+		// 	exibirLista.then(livroRetornado => {
+		// 		let oModel = new JSONModel(livroRetornado);
+		// 		this.getView().setModel(oModel, "livro")
+		// 	})
+		// },
 		
 
 		botaoEditar: function(){
@@ -34,34 +66,23 @@ sap.ui.define([
 		},
 
 
-		botaoSalvar: function(){
+		// botaoSalvar: function(){
 			
-			var telaCadastro = this.getView(),
-				inputs = [
-					telaCadastro.byId("input-nome"),
-					telaCadastro.byId("input-autor"),
-					telaCadastro.byId("input-editora"),
-				]
-			// var livroSalvar =this.oView.getModel(),
-			// 	oData = livroSalvar.getProperty("/listaDeLivros"),
-			// 	tabelaLivro = this.byId("listaDeLivros").getModel().getData();
-
-			// oData.push(tabelaLivro);
-			// tabelaLivro.setProperty("/listaDeLivros", oData);
+		// 	var telaCadastro = this.getView(),
+		// 		inputs = [
+		// 			telaCadastro.byId("input-nome"),
+		// 			telaCadastro.byId("input-autor"),
+		// 			telaCadastro.byId("input-editora"),
+		// 		]
+		// 		return this.cadastrarLivro;
 			
-		},
+			
+		// },
 
-		// cadastrarLivro: function () {
-		// 	var cadastrar = this.getView().getModel("livro").getData();
-		// 	return MessageBox.confirm("Deseja concluir o cadastro?", {
-		// 		title: "Confirmação",
-		// 		emphasizedAction: sap.m.MessageBox.Action.OK,
-		// 		actions: [sap.m.MessageBox.Action.OK,
-		// 			sap.m.MessageBox.Action.CANCEL
-		// 		],
-		// 		onClose: async function (oAction) {
-		// 			if (oAction === 'OK') {
-		// 				await fetch(`https://localhost:7278/CrudLivro`, {
+		// cadastrarLivro: async function(oAction){
+		// 	let cadastrar = this.getView().getModel("listaDeLivros").getData();
+		// 	if (oAction === 'Ok'){
+		// 		await fetch(`https://localhost:7278/CrudLivro`, {
 		// 					headers: {
 		// 						"Content-Type": "application/json; charset=utf-8"
 		// 					},
@@ -73,17 +94,13 @@ sap.ui.define([
 		// 						anoPublicacao: cadastrar.anoPublicacao,
 		// 					})
 		// 				})
-
-		// 				let oRouter = this.getOwnerComponent().getRouter();
+		// 	}
+		// 	let oRouter = this.getOwnerComponent().getRouter();
 		// 				oRouter.navTo("detalhes", {
 		// 					id: cadastrar.id
 		// 				});
-		// 			}
-		// 		},
-		// 	})
-		// },
 
-		
+		// }
 
 
 
